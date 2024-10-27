d9cd504108a6ead168173d338809b19fd51baf2e
Laszlo Nagy <laszlo.n620@gmail.com>
2024-10-27 15:17:58 +0100

    WSDL first projekt elkészült

---
+++ wsdlfirstws/src/test/java/org/laci/ws/soap/wsdlfirstws/WsdlfirstwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laci.ws.soap.wsdlfirstws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlfirstwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
