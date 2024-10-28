412d9dea9e4f9d245aa1a746c1a1b65f32860d7d
Laszlo Nagy <laszlo.n620@gmail.com>
2024-10-28 22:31:31 +0100

    WSDL2JAVA Java osztályok generálása

---
+++ wsdlfirstws/src/main/java/org/laci/ws/soap/wsdlfirstws/CustomerOrdersWsImpl.java
@@ -0,0 +1,15 @@
+package org.laci.ws.soap.wsdlfirstws;
+
+import org.laci.ws.soap.trainings.*;
+
+public class CustomerOrdersWsImpl implements CustomerOrdersPortType{
+    @Override
+    public GetOrdersResponse getOrders(GetOrdersRequest parameters) {
+        return null;
+    }
+
+    @Override
+    public CreateOrdersResponse createOrders(CreateOrdersRequest parameters) {
+        return null;
+    }
+}
