c499eec45529b81b3144930fb47fe3baa2fcf1ef
Laszlo Nagy <laszlo.n620@gmail.com>
2024-10-29 21:20:38 +0100

    JAVA SOAP CLIENT projekt elkészítése

---
+++ javasoapclient/src/test/java/org/laci/ws/soap/javasoapclient/JavasoapclientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laci.ws.soap.javasoapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasoapclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
